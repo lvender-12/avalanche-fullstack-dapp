d32e48512ca3970d4a79491b3afacbb4ef88f0cb
unknown <muhamaddamar1812@gmail.com>
2026-01-16 14:59:54 +0700

    update

--- dapps/frontend day 3/my-app/next.config.ts
+++ dapps/frontend day 3/my-app/next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    turbo: {
+      root: __dirname, // pakai root folder project
+    },
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
